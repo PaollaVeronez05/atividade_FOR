d9473c95344a3c092af171c5b4559043e05df156
PaollaVeronez05 <paollap.veronez@gmail.com>
2026-02-26 15:23:03 -0300

    Add files via upload

---
+++ ativ.for/script.js
@@ -0,0 +1,18 @@
+function gerarTabuadas() {
+  let resultadoDiv = document.getElementById("resultado");
+
+  resultadoDiv.innerHTML = "";
+
+  for (let numero = 1; numero <= 10; numero++) {
+    let tabuadaHTML = "<div class='card'>";
+    tabuadaHTML += "<h3>Tabuada do " + numero + "</h3>";
+
+    for (let i = 1; i <= 10; i++) {
+      tabuadaHTML += numero + " x " + i + " = " + numero * i + "<br>";
+    }
+
+    tabuadaHTML += "</div>";
+
+    resultadoDiv.innerHTML += tabuadaHTML;
+  }
+}
